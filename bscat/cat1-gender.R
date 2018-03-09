7455d57c8b433e25c43c37003657e3165f9efaa2
tutrunghieu <tutrunghieu@gmail.com>
2018-03-09 09:58:42 +0800

    update

--- bscat/cat1-gender.R
+++ bscat/cat1-gender.R
@@ -2,19 +2,28 @@ rm(list=ls());
 set.seed(197);
 
 main <- function() {
+
+#----- gender
 n <- 1000;
 G <- ifelse(runif(n)<0.7, "Male", "Female");
 
+#----- role
 U <- runif(n);
 R <- ifelse(U<0.15, "Director", ifelse(U<0.15+0.3, "Manager", "Worker"));
 
+#----- job title
+nX <- c("Engineer", "Business", "Admin", "Driver", "Cleaner");
+pX <- c(3, 4, 5, 2, 7); pX <- pX / sum(pX);
+P <- sample(nX, prob=pX, size=n, replace=TRUE);  
 
-
-df <- data.frame(Gender=G, Role=R);
+#----- combining
+df <- data.frame(Gender=G, Role=R, Profession=P);
 print(head(df));
 
+#----- testing
 print( table(df$Gender) );
 print( table(df$Role) );
+print( table(df$Profession) );
 }
 
 main();
