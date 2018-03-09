8ce956c5318a613bef2aa9c63bcd0e443167c33a
tutrunghieu <tutrunghieu@gmail.com>
2018-03-09 09:46:22 +0800

    Adding

--- bscat/cat1-gender.R
+++ bscat/cat1-gender.R
@@ -1,2 +1,20 @@
 rm(list=ls());
+set.seed(197);
 
+main <- function() {
+n <- 1000;
+G <- ifelse(runif(n)<0.7, "Male", "Female");
+
+U <- runif(n);
+R <- ifelse(U<0.15, "Director", ifelse(U<0.15+0.3, "Manager", "Worker"));
+
+
+
+df <- data.frame(Gender=G, Role=R);
+print(head(df));
+
+print( table(df$Gender) );
+print( table(df$Role) );
+}
+
+main();
