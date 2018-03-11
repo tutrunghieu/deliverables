ecd21eabbcf7ce13ca5e350f09ea5e4f5364ec2b
tutrunghieu <tutrunghieu@gmail.com>
2018-03-11 13:43:56 +0800

    Cleaning

--- bsnum/num1-range.R
+++ bsnum/num1-range.R
@@ -13,19 +13,25 @@ G1 <- rgamma(n, shape=0.2);
 G2 <- rgamma(n, shape=0.4);
 A2 <- round( (G1 * 18 + G2 * 80) / (G1 + G2) );
 
-#----- age via dirichlet
-A3 <- rnorm(n, mean=35, sd=3);
+#----- age via normal
+A3 <- round( rnorm(n, mean=35, sd=3) );
+
+#----- age via mixture of gaussian
+X1 <-  rnorm(n, mean=5, sd=2);
+X2 <-  rnorm(n, mean=60, sd=5);
+sel <- ifelse(runif()<0.7, 1, 0);
+A4 <- X1*sel + X2*(1-sel);
 
 
 #----- combining
-df <- data.frame(Age.Range=A1, Age.Dirichlet=A2, Age.Around=A3);
+df <- data.frame(Age.Range=A1, Age.Dir=A2, Age.Around=A3);
 print(head(df), row.names=F);
 
 #----- testing
 hist(df$Age.Range, breaks=50);
 
 dev.new();
-hist(df$Age.Dirichlet, breaks=50);
+hist(df$Age.Dir, breaks=50);
 
 dev.new();
 hist(df$Age.Around, breaks=50);
